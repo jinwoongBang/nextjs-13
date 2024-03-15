466906b7be9fef23fd332a40f252eb18e6f95476
Jinwoong Bang <jinwoongbang@Jinwoongs-MacBook-Pro.local>
2024-03-15 16:40:13 +0900

    feature : 닫기 아이콘 추가 및 drawer 메뉴 닫기 버튼 추가

--- nextjs13-quiz/components/Header.tsx
+++ nextjs13-quiz/components/Header.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import { XCircleIcon } from "@heroicons/react/24/outline";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [open, setOpen] = useState(true);
+
+  const routes = [
+    { title: "Home", path: "/" },
+    { title: "Question", path: "/question" },
+    { title: "State", path: "/state" },
+    { title: "Quiz", path: "/quiz" },
+  ];
+  return (
+    <header className="shadow-lg">
+      <div
+        className={`fixed lg:hidden transition-all z-20 duration-300 ${
+          open ? "right-0" : "-right-64"
+        } p-4 bg-orange-500 h-screen w-64`}
+      >
+        <XCircleIcon
+          className={`w-8 h-8 stroke-slate-50 mb-4`}
+          stroke="2"
+          onClick={() => setOpen(false)}
+        ></XCircleIcon>
+        <div className="flex flex-col space-y-4" onClick={() => setOpen(false)}>
+          {routes.map((route) => (
+            <Link
+              className="px-6 py-2 text-slate-100 rounded bg-orange-700"
+              href={route.path}
+              key={route.path}
+            >
+              {route.title}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
