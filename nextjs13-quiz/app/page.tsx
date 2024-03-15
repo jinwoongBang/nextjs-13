7b956acb9ebb391ddf44689cc59b50443b558859
Jinwoong Bang <jinwoongbang@Jinwoongs-MacBook-Pro.local>
2024-03-15 16:23:25 +0900

    feature : 헤더 스타일 지정

--- nextjs13-quiz/app/page.tsx
+++ nextjs13-quiz/app/page.tsx
@@ -2,7 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="max-w-7xl overlow-hidden rounded-xl p-4 lg:p-8 text-slate-700">
+    <div
+      className={
+        "max-w-7xl overlow-hidden rounded-xl p-4 lg:p-8 text-slate-700"
+      }
+    >
       HomePage
     </div>
   );
