bae705398f8008b716f1f013c1b02648625e2839
Jinwoong Bang <jinwoongbang@Jinwoongs-MacBook-Pro.local>
2024-03-15 16:23:07 +0900

    feature : 기본 스타일 지정

--- nextjs13-quiz/app/page.tsx
+++ nextjs13-quiz/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>HomePage</div>;
+  return (
+    <div className="max-w-7xl overlow-hidden rounded-xl p-4 lg:p-8 text-slate-700">
+      HomePage
+    </div>
+  );
 }
