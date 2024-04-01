fd1e2b6b20dc6dda2f5df1ef18aef81ad1033f45
Jinwoong Bang <jinwoongbang@Jinwoongs-MacBook-Pro.local>
2024-04-01 22:54:23 +0900

    feature : weather 페이지 생성 및 비로그인 페이지 생성

--- nextjs12-weather/pages/unauthenticated.tsx
+++ nextjs12-weather/pages/unauthenticated.tsx
@@ -1,7 +1,16 @@
+import Layout from "@/components/Layout";
+import Link from "next/link";
 import React from "react";
 
 function Unauthenticated() {
-  return <div>Unauthenticated</div>;
+  return (
+    <Layout>
+      <h1>이 페이지를 보려면 로그인을 하세요.</h1>
+      <Link href="/" className="mx-auto btn__secondary">
+        로그인 페이지로
+      </Link>
+    </Layout>
+  );
 }
 
 export default Unauthenticated;
--- nextjs12-weather/pages/weather.tsx
+++ nextjs12-weather/pages/weather.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 function Weather() {
-  return <div>weather</div>;
+  const [currentWeather, setCurrentWeather] = useState(null);
+
+  const [city, setCity] = useState("Seoul");
+  const [tempIsLow, setTempIsLow] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  return <div>weather</div>
 }
 
 export default Weather;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/unauthenticated",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
