b14c79ab4287cb89e93639a7d679ff17192cd786
Jinwoong Bang <jinwoongbang@Jinwoongs-MacBook-Pro.local>
2024-04-04 21:42:36 +0900

    feature : 도시 데이터 요청 기능 추가"

--- nextjs13-weather/components/CitySearchBar/CitySearchBar.tsx
+++ nextjs13-weather/components/CitySearchBar/CitySearchBar.tsx
@@ -1,6 +1,44 @@
 import React, { useEffect, useState } from "react";
 
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { WeatherDbData } from "@/models/weatherDbData";
+
 const CitySearchBar = () => {
+  const [search, setSearch] = useState("");
+  // const debouncedSearch = useDebounce(search, 1000);
+  const [isShowingSearchResults, setIsShowingSearchResults] = useState(false);
+
+  const cities = useQuery({
+    queryFn: async () => {
+      if (!search) {
+        setIsShowingSearchResults(false);
+        return [];
+      }
+      try {
+        const fetchedCities = await axios.get<WeatherDbData[]>(
+          `/api/cities/${search}`
+        );
+        if (fetchedCities.status !== 200) {
+          console.error(fetchedCities);
+          return [];
+        }
+        setIsShowingSearchResults(true);
+
+        return fetchedCities.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    queryKey: [search.toLowerCase()],
+    refetchOnMount: true,
+    refetchOnWindowFocus: false,
+  });
+
+  useEffect(() => {
+    cities.refetch();
+  }, [search]);
+
   return <div className="searchBar"></div>;
 };
 
