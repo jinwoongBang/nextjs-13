d1720b13f76fbdf1c2fa6254b602e4da6744bc32
Jinwoong Bang <jinwoongbang@Jinwoongs-MacBook-Pro.local>
2024-04-04 21:50:41 +0900

    feature : 도시 데이터 API 구현

---
+++ nextjs13-weather/app/api/cities/[citiNamePart]/route.ts
@@ -0,0 +1,22 @@
+import citiesData from "@/data/cities.json";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  { params: { cityNamePart } }: { params: { cityNamePart: string } }
+) {
+  const noRepeatCitiesNames: string[] = [];
+  const filteredCities = citiesData.filter((city) => {
+    if (
+      noRepeatCitiesNames.includes(city.name) ||
+      !city.name.toLowerCase().includes(cityNamePart.toLowerCase())
+    ) {
+      return false;
+    }
+    noRepeatCitiesNames.push(city.name);
+
+    return true;
+  });
+
+  return NextResponse.json(filteredCities);
+}
