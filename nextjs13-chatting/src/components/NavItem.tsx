3b0749a1221c261f901bddb6c78ca2de955c8ba6
Jinwoong Bang <jinwoongbang@Jinwoongs-MacBook-Pro.local>
2024-04-09 20:21:38 +0900

    feature : 로그인 로그아웃 jwt 구현

--- nextjs13-chatting/src/components/NavItem.tsx
+++ nextjs13-chatting/src/components/NavItem.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
 import React from "react";
 
+import { signIn, signOut, useSession } from "next-auth/react";
+
 interface NavItemProps {
   mobile?: boolean;
 }
 
 const NavItem = ({ mobile }: NavItemProps) => {
+  const { data: session, status } = useSession();
+  console.debug({ session }, status);
+
   return (
     <ul
       className={`text-md justify-center flex w-full gap-4 ${
@@ -18,12 +23,15 @@ const NavItem = ({ mobile }: NavItemProps) => {
       <li className={`py-2 text-center border-b-4 cursor-pointer`}>
         <Link href={"/user"}>User</Link>
       </li>
-      <li className={`py-2 text-center border-b-4 cursor-pointer`}>
-        <button>Signout</button>
-      </li>
-      <li className={`py-2 text-center border-b-4 cursor-pointer`}>
-        <button>Signin</button>
-      </li>
+      {session?.user ? (
+        <li className={`py-2 text-center border-b-4 cursor-pointer`}>
+          <button onClick={() => signOut()}>Signout</button>
+        </li>
+      ) : (
+        <li className={`py-2 text-center border-b-4 cursor-pointer`}>
+          <button onClick={() => signIn()}>Signin</button>
+        </li>
+      )}
     </ul>
   );
 };
