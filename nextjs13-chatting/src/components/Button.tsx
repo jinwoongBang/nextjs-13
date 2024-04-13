f459f62c00a3a0257bb582eeb389a7fb91d3f475
Jinwoong Bang <jinwoongbang@Jinwoongs-MacBook-Pro.local>
2024-04-13 10:41:31 +0900

    feature : 공통 버튼 컴포넌트 구현

--- nextjs13-chatting/src/components/Button.tsx
+++ nextjs13-chatting/src/components/Button.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+import { IconType } from "react-icons";
+import { RotatingLines } from "react-loader-spinner";
+
 interface ButtonProps {
   label?: string;
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
@@ -19,7 +22,46 @@ const Button: React.FC<ButtonProps> = ({
   icon: Icon,
   isLoading = false,
 }) => {
-  return <div>Button</div>;
+  return (
+    <button
+      type="submit"
+      disabled={disabled}
+      onClick={onClick}
+      className={`
+        relative
+        disabled:opacity-70
+        disabled:cursor-not-allowed
+        rounded-lg
+        hover:opacity-80
+        transition   
+        w-full
+        ${outline ? "bg-white" : "bg-orange-500"}
+        ${outline ? "border-black" : "border-orange-500"}
+        ${outline ? "text-black" : "text-white"}
+        ${small ? "text-sm" : "text-md"}
+        ${small ? "py-1" : "py-3"}
+        ${small ? "font-light" : "font-semibold"}
+        ${small ? "border-[1px]" : "border-2"}
+        `}
+    >
+      {isLoading ? (
+        <div className="flex justify-center">
+          <RotatingLines
+            strokeColor="grey"
+            strokeWidth="5"
+            animationDuration="0.75"
+            width="30"
+            visible={true}
+          />
+        </div>
+      ) : (
+        <>
+          {Icon && <Icon size={24} className="absolute left-4 top-3" />}
+          {label && label}
+        </>
+      )}
+    </button>
+  );
 };
 
 export default Button;
