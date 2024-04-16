675e593fb6edfc8c0a3ac1903ee8c9951db3558d
Jinwoong Bang <jinwoongbang@Jinwoongs-MacBook-Pro.local>
2024-04-16 21:02:56 +0900

    feature : 로그인 & 획원가입 UI 구현

--- nextjs13-chatting-app/src/app/(home)/components/AuthForm.tsx
+++ nextjs13-chatting-app/src/app/(home)/components/AuthForm.tsx
@@ -9,6 +9,11 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import axios from "axios";
 import { toast } from "react-hot-toast";
+import { BsGithub, BsGoogle } from "react-icons/bs";
+
+import Input from "@/components/inputs/Input";
+import Button from "@/components/Button";
+import AuthSocialButton from "@/app/(home)/components/AuthSocialButton";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -104,7 +109,79 @@ const AuthForm = () => {
       .finally(() => setIsLoading(false));
   };
 
-  return <div>AuthForm</div>;
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGISTER" && (
+            <Input
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+              id="name"
+              label="이름"
+            />
+          )}
+          <Input
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+            id="email"
+            label="이메일"
+            type="email"
+          />
+          <Input
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+            id="password"
+            label="비밀번호"
+            type="password"
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "로그인" : "회원가입"}
+            </Button>
+          </div>
+        </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center ">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="px-2 text-gray-500 bg-white">소셜 로그인</span>
+            </div>
+          </div>
+
+          <div className="flex gap-2 mt-6">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+          </div>
+        </div>
+        <div className="flex justify-center gap-2 px-2 mt-6 text-sm text-gray-500 ">
+          <div>
+            {variant === "LOGIN"
+              ? "메신저를 처음 사용하시나요?"
+              : "이미 계정이 있나요?"}
+          </div>
+          <div onClick={toggleVariant} className="underline cursor-pointer">
+            {variant === "LOGIN" ? "계정 만들기" : "로그인하기"}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AuthForm;
