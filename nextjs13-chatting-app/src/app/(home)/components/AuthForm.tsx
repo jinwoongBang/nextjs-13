69445584981edac9f35a82d8bff557e66672253e
Jinwoong Bang <jinwoongbang@Jinwoongs-MacBook-Pro.local>
2024-04-16 20:45:02 +0900

    feature : 회원가입, 로그인 기능 구현

---
+++ nextjs13-chatting-app/src/app/(home)/components/AuthForm.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import React, { useCallback, useEffect, useState } from "react";
+
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
+type Variant = "LOGIN" | "REGISTER";
+
+const AuthForm = () => {
+  const session = useSession();
+  const router = useRouter();
+
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (session?.status === "authenticated") {
+      router.push("/conversations");
+    }
+  }, [session?.status, router]);
+
+  const toggleVariant = () => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === "REGISTER") {
+      axios
+        .post("/api/register", data)
+        .then(() =>
+          signIn("credentials", {
+            ...data,
+            redirect: false,
+          })
+        )
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error("Invalid credentials!");
+          }
+
+          if (callback?.ok) {
+            router.push("/conversations");
+          }
+        })
+        .catch(() => toast.error("Something went wrong!"))
+        .finally(() => setIsLoading(false));
+    }
+
+    if (variant === "LOGIN") {
+      signIn("credentials", {
+        ...data,
+        redirect: false,
+      })
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error("Invalid credentials!");
+          }
+
+          if (callback?.ok) {
+            router.push("/conversations");
+          }
+        })
+        .finally(() => setIsLoading(false));
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+
+    signIn(action, { redirect: false })
+      .then((callback) => {
+        if (callback?.error) {
+          toast.error("Invalid credentials!");
+        }
+
+        if (callback?.ok) {
+          router.push("/conversations");
+        }
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  return <div>AuthForm</div>;
+};
+
+export default AuthForm;
