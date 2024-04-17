0eaf42c24f30af9023399b3e0db6e0506a58acec
Jinwoong Bang <jinwoongbang@Jinwoongs-MacBook-Pro.local>
2024-04-17 22:12:35 +0900

    feature : mobile Footer 생성

--- nextjs13-chatting-app/src/components/sidebar/MobileFooter.tsx
+++ nextjs13-chatting-app/src/components/sidebar/MobileFooter.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+"use client";
+
+import useConversation from "@/hooks/useConversation";
+import useRoutes from "@/hooks/useRoutes";
+import MobileItem from "./MobileItem";
 
 const MobileFooter = () => {
-  return <div>MobileFooter</div>;
+  const routes = useRoutes();
+  const { isOpen } = useConversation();
+
+  if (isOpen) {
+    return null;
+  }
+
+  return (
+    <div
+      className="
+        fixed 
+        justify-between 
+        w-full 
+        bottom-0 
+        z-40 
+        flex 
+        items-center 
+        bg-white 
+        border-t-[1px] 
+        lg:hidden
+      "
+    >
+      {routes.map(({ href, active, icon, onClick }) => (
+        <MobileItem
+          key={href}
+          href={href}
+          active={active}
+          icon={icon}
+          onClick={onClick}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default MobileFooter;
