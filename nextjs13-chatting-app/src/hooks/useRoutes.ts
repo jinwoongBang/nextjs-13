09bbf5d028245f758758d2b049b6eb6c90ba051a
Jinwoong Bang <jinwoongbang@Jinwoongs-MacBook-Pro.local>
2024-04-17 21:55:46 +0900

    feature : sidebar 의 활성화 상태 체크를 위한 custom hook 추가

---
+++ nextjs13-chatting-app/src/hooks/useRoutes.ts
@@ -0,0 +1,41 @@
+import { useMemo } from "react";
+import { usePathname } from "next/navigation";
+import { signOut } from "next-auth/react";
+
+import { HiChat } from "react-icons/hi";
+import { HiArrowLeftOnRectangle, HiUsers } from "react-icons/hi2";
+
+import useConversation from "./useConversation";
+
+const useRoutes = () => {
+  const pathname = usePathname();
+  const { conversationId, isOpen } = useConversation();
+
+  const routes = useMemo(
+    () => [
+      {
+        label: "Chat",
+        href: "/conversations",
+        icon: HiChat,
+        active: pathname === "/conversations" || isOpen,
+      },
+      {
+        label: "Users",
+        href: "/users",
+        icon: HiUsers,
+        active: pathname === "/users",
+      },
+      {
+        label: "Logout",
+        onClick: () => signOut(),
+        href: "#",
+        icon: HiArrowLeftOnRectangle,
+      },
+    ],
+    [pathname, conversationId]
+  );
+
+  return routes;
+};
+
+export default useRoutes;
