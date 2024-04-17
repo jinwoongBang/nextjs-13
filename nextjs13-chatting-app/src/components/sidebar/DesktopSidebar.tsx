422115cc806aefe1a8f27e95c6c06056b4a43c94
Jinwoong Bang <jinwoongbang@Jinwoongs-MacBook-Pro.local>
2024-04-17 22:01:08 +0900

    feature : 사이드바 UI 구현

--- nextjs13-chatting-app/src/components/sidebar/DesktopSidebar.tsx
+++ nextjs13-chatting-app/src/components/sidebar/DesktopSidebar.tsx
@@ -1,11 +1,66 @@
-import { User } from "next-auth";
-import React from "react";
+"use client";
 
-type DesktopSidebarProps = {
+import { useState } from "react";
+import { User } from "next-auth";
+
+import useRoutes from "@/hooks/useRoutes";
+
+import DesktopItem from "./DesktopItem";
+import Avatar from "../Avatar";
+
+interface DesktopSidebarProps {
   currentUser: User;
-};
-const DesktopSidebar = ({ currentUser }: DesktopSidebarProps) => {
-  return <div>DesktopSidebar</div>;
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
+  const routes = useRoutes();
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <div
+        className="
+        hidden 
+        lg:fixed  
+        lg:inset-y-0 
+        lg:left-0 
+        lg:z-40 
+        lg:w-20 
+        xl:px-6
+        lg:overflow-y-auto 
+        lg:bg-orange-400 
+        lg:border-r-[1px]
+        lg:pb-4
+        lg:flex
+        lg:flex-col
+        justify-between
+      "
+      >
+        <nav className="flex flex-col justify-between mt-4">
+          <ul role="list" className="flex flex-col items-center space-y-1">
+            {routes.map((item) => (
+              <DesktopItem
+                key={item.label}
+                href={item.href}
+                label={item.label}
+                icon={item.icon}
+                active={item.active}
+                onClick={item.onClick}
+              />
+            ))}
+          </ul>
+        </nav>
+        <nav className="flex flex-col items-center justify-between mt-4">
+          <div
+            onClick={() => setIsOpen(true)}
+            className="transition cursor-pointer hover:opacity-75"
+          >
+            <Avatar />
+          </div>
+        </nav>
+      </div>
+    </>
+  );
 };
 
 export default DesktopSidebar;
