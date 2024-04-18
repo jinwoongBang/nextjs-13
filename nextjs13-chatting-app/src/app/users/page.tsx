21dc53b1168d027d22697a7e341914f84ab297f6
Jinwoong Bang <jinwoongbang@Jinwoongs-MacBook-Pro.local>
2024-04-18 18:37:47 +0900

    feature : User 목록 페이지 생성

--- nextjs13-chatting-app/src/app/users/page.tsx
+++ nextjs13-chatting-app/src/app/users/page.tsx
@@ -1,7 +1,12 @@
+import EmptyState from "@/components/EmptyState";
 import React from "react";
 
 const UsersPage = () => {
-  return <div>UsersPage</div>;
+  return (
+    <div className="hidden h-full lg:block lg:pl-80">
+      <EmptyState />
+    </div>
+  );
 };
 
 export default UsersPage;
