b63c057d155768139325a44d4f68905728e9c989
jinwoongBang <dlopo123@gmail.com>
2024-05-21 22:52:16 +0900

    feature : 그룹 채팅 UI 추가

--- nextjs13-chatting-app/src/components/modals/GroupChatModal.tsx
+++ nextjs13-chatting-app/src/components/modals/GroupChatModal.tsx
@@ -57,7 +57,59 @@ const GroupChatModal: React.FC<GroupChatModalProps> = ({
       .finally(() => setIsLoading(false));
   };
 
-  return <></>;
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="space-y-12">
+          <div className="pb-12 border-b border-gray-900/10">
+            <h2 className="text-base font-semibold leading-7 text-gray-900 ">
+              그룹 채팅 만들기
+            </h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              2명 이상의 사람들과 채팅을 만드세요.
+            </p>
+            <div className="flex flex-col mt-10 gap-y-8">
+              <Input
+                disabled={isLoading}
+                label="Name"
+                id="name"
+                errors={errors}
+                required
+                register={register}
+              />
+              <Select
+                disabled={isLoading}
+                label="Members"
+                options={users.map((user) => ({
+                  value: user.id,
+                  label: user.name,
+                }))}
+                onChange={(value) =>
+                  setValue("members", value, {
+                    shouldValidate: true,
+                  })
+                }
+                value={members}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center justify-end mt-6 gap-x-6">
+          <Button
+            disabled={isLoading}
+            onClick={onClose}
+            type="button"
+            secondary
+          >
+            취소
+          </Button>
+          <Button disabled={isLoading} type="submit">
+            생성
+          </Button>
+        </div>
+      </form>
+    </Modal>
+  );
 };
 
 export default GroupChatModal;
